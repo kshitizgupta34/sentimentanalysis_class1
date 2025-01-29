b9205b26b887aa40d11400f47243e96738452472
kshitizgupta34 <kshitizgupta34@gmail.com>
2025-01-29 14:45:16 -0500

    sentiment

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+st.title('California Hosing Price Prediction')
+st.write('Trial 1')
+
+model = pickle.load(open('model_lr.pkl','rb'))
+
+med_inc = st.number_input('Median Income')
+
+st.button('Predict')
